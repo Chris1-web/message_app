5abd5a595283be5981bae31e2059e1bdf38b3787
Christianah Adeyemo <christianah123.ac@gmail.com>
2023-09-02 15:10:39 -0700

    create env file to store database information

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 const app = express();
+
+// Set up mongoose connection
+mongoose.set("strictQuery", false);
+const mongoDB = process.env.MONGODB_URL;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
 
 app.use(cors());
 
@@ -10,4 +21,5 @@ app.post("/accounts", (req, res) => {
 
 app.listen(3000, () => {
   console.log("app listen at port 3000");
+  console.log(process.env.MONGODB_URL);
 });
