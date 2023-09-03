50d3e26e53a360fce6f71a9576714b1af5ba5ec4
Christianah Adeyemo <christianah123.ac@gmail.com>
2023-09-02 23:46:27 -0700

    create login route to get token

--- backend/controllers/accountControllers.js
+++ backend/controllers/accountControllers.js
@@ -1,5 +1,6 @@
 const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 // model
 const Account = require("../models/account");
@@ -10,7 +11,9 @@ exports.account_create_post = [
     .isLength({ min: 3, max: 100 })
     .escape()
     .custom(async (value) => {
-      const existingUser = await Account.findOne({ username: value });
+      const existingUser = await Account.findOne({
+        username: value.toLowerCase(),
+      });
       if (existingUser) {
         throw new Error("Username already exists");
       }
@@ -54,5 +57,34 @@ exports.account_create_post = [
         return res.status(500).json({ errors: error.errors });
       }
     });
+  },
+];
+
+exports.account_login = [
+  body("username", "Username is required").trim().isLength({ min: 1 }).escape(),
+  body("password", "Password is required").trim().isLength({ min: 1 }).escape(),
+  async (req, res) => {
+    const { username, password } = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      // check if user exists in database and password is correct
+      const user = await Account.findOne({ username: username.toLowerCase() });
+      if (!user) throw new Error("User does not exist");
+      const passwordCheck = await bcrypt.compare(password, user.password);
+      if (!passwordCheck) throw new Error("Incorrect password");
+
+      // create a token to be used in consecutive requests
+      const token = jwt.sign({ user }, process.env.JWT_KEY, {
+        expiresIn: "7h",
+      });
+      return res.status(200).json({ token });
+    } catch (error) {
+      console.log(error.message);
+      return res.status(400).json({ error: { message: error.message } });
+    }
   },
 ];
--- backend/routes/api.js
+++ backend/routes/api.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const accountController = require("../controllers/accountControllers");
 
 router.post("/", accountController.account_create_post);
+router.post("/login", accountController.account_login);
 
 module.exports = router;
